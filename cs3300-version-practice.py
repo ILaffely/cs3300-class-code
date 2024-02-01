a7c43da4e1e342de121b2916825fade2d16e29e7
Isaac Laffely <laffely.isaac@gmail.com>
2024-01-31 20:30:35 -0700

    Trying to add this to a different branch

--- cs3300-version-practice.py
+++ cs3300-version-practice.py
@@ -1,2 +1,3 @@
 #This is a test folder that is used to learn about version control 
 #The purpose of this comment is to change this file so that I can have a change to push
+#another comment to test if I can create different branches
